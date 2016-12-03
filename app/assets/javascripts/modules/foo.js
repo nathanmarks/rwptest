8f8768aa9e73516c461561b73dbca3c7788a1030
nathanmarks <info@nathanmarks.io>
2016-12-03 17:08:32 -0500

    hmr

---
+++ app/assets/javascripts/modules/foo.js
@@ -0,0 +1,5 @@
+export default function foo() {
+  const foo = document.createElement('p');
+  foo.innerHTML = 'hello world';
+  return foo;
+}
--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -2,19 +2,18 @@
 
 const path = require('path');
 const webpack = require('webpack');
+const StatsPlugin = require('stats-webpack-plugin');
 
 // must match config.webpack.dev_server.port
 const DEV_SERVER_PORT = 3808;
 
 const ROOT_PATH = path.resolve(__dirname, '../');
-const ASSETS_JS_PATH = path.resolve(ROOT_PATH, 'app/assets/javascripts-next');
+const ASSETS_JS_PATH = path.resolve(ROOT_PATH, 'app/assets/javascripts');
 
 module.exports = {
-  debug: true,
   devtool: 'inline-source-map',
   entry: {
     application: [
-      'eventsource-polyfill', // hot reloading in IE... ¯\_(ツ)_/¯
       `webpack-dev-server/client?http://0.0.0.0:${DEV_SERVER_PORT}`,
       'webpack/hot/only-dev-server',
       `${ASSETS_JS_PATH}/application.js`,
@@ -34,61 +33,17 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loaders: ['babel'],
-      },
-      {
-        test: /\.css$/,
-        loader: 'style!css',
+        loader: 'babel-loader',
       },
     ],
   },
-  progress: true,
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: "'development'",
       },
     }),
-    {
-      apply(compiler) {
-        compiler.plugin('emit', (compilation, done) => {
-          let result;
-
-          const stats = compilation.getStats().toJson({
-            // node_modules/webpack/lib/Stats.js
-            hash: true,
-            version: true,
-            timings: false,
-            assets: true,
-            chunks: false,
-            chunkModules: false,
-            chunkOrigins: false,
-            modules: false,
-            cached: false,
-            reasons: false,
-            children: false,
-            source: false,
-            errors: false,
-            errorDetails: false,
-            warnings: false,
-            publicPath: true,
-          });
-          delete stats.assets;
-
-          compilation.assets['manifest.json'] = {
-            size: function getSize() {
-              return result ? result.length : 0;
-            },
-            source: function getSource() {
-              result = JSON.stringify(stats);
-              return result;
-            },
-          };
-
-          done();
-        });
-      },
-    },
+    new StatsPlugin('manifest.json'),
     new webpack.NamedModulesPlugin(),
     new webpack.HotModuleReplacementPlugin(),
   ],
